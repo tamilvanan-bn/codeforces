1e3838105e3b9e7f8e6494a2a5d64b620ec616bc
Tamilvanan B <tamilvanan.bn@gmail.com>
2025-04-21 06:39:09 +0530

    [38] [Time Beats: 35.30%] [Memory Beats: 66.69%] - LeetPush

--- Count-and-Say.cpp
+++ Count-and-Say.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
 
-    std::string count(const std::string& str) {
-        int n = str.size();
-        std::string res = "";
+    std::string count(const std::string& s) {
+        int n = s.size();
+        std::string ans = "";
 
         for (int i = 0; i < n; ) {
-            char current_char = str[i];
+            char current_char = s[i];
             int count = 1;
 
-            while (i + count < n && str[i + count] == current_char) {
-                count++;
-            }
-
-            res += std::to_string(count) + current_char;
+            while (i + count < n && s[i + count] == current_char) count++;
+            ans += std::to_string(count) + current_char;
             i += count;
         }
-        
-        return res;
+
+        return ans;
     }
 
     string countAndSay(int n) {
         std::string res = "1";
-        if (n == 1) return "1";
+        if (n == 1) return res;
 
         for (int i = 2; i <= n; i++) {
             res = count(res);
