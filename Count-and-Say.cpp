517205c9bd96858f307f5f23fc35637dde7bf777
Tamilvanan B <tamilvanan.bn@gmail.com>
2025-04-21 07:55:11 +0530

    Solution2 Count-and-Say.cpp

--- Count-and-Say.cpp
+++ Count-and-Say.cpp
@@ -28,3 +28,30 @@ public:
         return res;
     }
 };
+
+class Solution2 {
+public:
+
+    string countAndSay(int n) {
+        std::string res = "1";
+        if (n == 1) return res;
+
+        for (int i = 1; i < n; i++) {
+            std::string current = "";
+            int count = 1;
+
+            for (int j = 1; j < res.size(); j++) {
+                if (res[j] == res[j - 1]) count++;
+                else {
+                    current += std::to_string(count) + res[j - 1];
+                    count = 1;
+                }
+            }
+
+            current += std::to_string(count) + res.back();
+            res = current;
+        }
+
+        return res;
+    }
+};
