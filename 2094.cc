da17eb640671dfd76c4bec74cb336c003f5332b3
Tamilvanan BN <tamilvanan.bn@gmail.com>
2025-05-02 14:58:08 +0530

    Create 2094.cc

---
+++ 2094.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        int n;
+        std::cin >> n;
+        int mat[n][n];
+        for (std::size_t i = 0; i < n; i++) {
+            for (std::size_t j = 0; j < n; j++) {
+                std::cin >> mat[i][j];
+            }
+        }
+
+        int res[n * 2] = {0}, j{0};
+        for (int i = n - 1; i >= 0; i--) {
+            res[j++] = mat[n - 1][i];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            res[j++] = mat[i][0];
+        }
+        n *= 2;
+        int sum = (n * (n + 1)) / 2, run_sum{0};
+        for (const int& i : res) run_sum += i;
+        sum = sum - run_sum;
+        res[n - 1] = sum;
+
+        for (int i = n - 1; i >= 0; i--) std::cout << res[i] << ' ';
+        std::cout << std::endl;
+    }
+    return 0;
+}
